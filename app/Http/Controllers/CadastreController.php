ed0da4cafe70afca31388069312905cf0f3dc126
Bruce Rodriguez <bruce.rodriguez94@gmail.com>
2024-11-05 12:59:07 -0500

    Se integro el uso de la libreria swagger para poder documentar el uso de la API

--- app/Http/Controllers/CadastreController.php
+++ app/Http/Controllers/CadastreController.php
@@ -8,6 +8,52 @@ use Illuminate\Http\Request;
 
 class CadastreController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/price-m2/zip-codes/{zipCode}/aggregate/{type}",
+     *     summary="Get aggregated price data",
+     *     description="Returns aggregated price data for a given zip code and type",
+     *     @OA\Parameter(
+     *         name="zipCode",
+     *         in="path",
+     *         required=true,
+     *         description="Zip code to fetch data for (string)",
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Parameter(
+     *         name="type",
+     *         in="path",
+     *         required=true,
+     *         description="Type of aggregation (min, max, avg)",
+     *         @OA\Schema(type="string", enum={"min", "max", "avg"})
+     *     ),
+     *     @OA\Parameter(
+     *         name="uso_construccion",
+     *         in="query",
+     *         required=false,
+     *         description="Usage construction parameter (1 to 7)",
+     *         @OA\Schema(type="integer", minimum=1, maximum=7, example=3)
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="status", type="boolean", example=true),
+     *             @OA\Property(property="payload", type="object",
+     *                 @OA\Property(property="type", type="string", example="avg"),
+     *                 @OA\Property(property="price_unit", type="integer", example=1420),
+     *                 @OA\Property(property="price_unit_construction", type="integer", example=3120),
+     *                 @OA\Property(property="elements", type="integer", example=100)
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Data not found",
+     *     )
+     * )
+     */
     public function getPriceM2(string $postalCode, $type, Request $request)
     {
         $cadastres = CadastreService::getFilteredCadastres($postalCode, $request->uso_construccion);
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,13 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     title="API Title",
+ *     version="1.0.0",
+ *     description="API description"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
